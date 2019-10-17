11c8e3e6b7b580e0b45219d1317c6d3a07e43f53
KnowProgrammer <34388782+KnowProgrammer@users.noreply.github.com>
2019-10-16 20:01:49 -0400

    main program to make audiobooks

---
+++ amazonpolly.py
@@ -0,0 +1,76 @@
+import boto3
+import os
+from contextlib import closing
+#from boto3.dynamodb.conditions import Key, Attr
+
+def pollytext(filename,pollyvoice):
+
+
+
+    with open(filename) as file:
+        text = file.read()
+
+    polly_client = boto3.Session(
+        aws_access_key_id='YOU GET YOUR AWS ACCESS KEY FROM AMAZON',
+        aws_secret_access_key='YOU GET YOUR AWS SECRET KEY FROM AMAZON',
+        region_name='us-east-1').client('polly')
+
+
+    sep = '.'
+    outputfilename = filename.split(sep, 1)[0]
+
+    voice = pollyvoice
+
+    rest = text
+
+    #Because single invocation of the polly synthesize_speech api can
+    # transform text with about 1,500 characters, we are dividing the
+    # post into blocks of approximately 1,000 characters.
+    textBlocks = []
+    while (len(rest) > 2100):
+        begin = 0
+        end = rest.find(".", 2000)
+
+        if (end == -1):
+            end = rest.find(" ", 2000)
+
+        textBlock = rest[begin:end]
+        rest = rest[end:]
+        textBlocks.append(textBlock)
+    textBlocks.append(rest)
+
+
+    #For each block, invoke Polly API, which will transform text into audio
+
+    for textBlock in textBlocks:
+        response = polly_client.synthesize_speech(
+            OutputFormat='mp3',
+            Text = textBlock,
+            VoiceId = voice,
+            Engine = 'neural'
+        )
+
+        #Save the audio stream returned by Amazon Polly on Lambda's temp
+        # directory. If there are multiple text blocks, the audio stream
+        # will be combined into a single file.
+
+
+
+        if "AudioStream" in response:
+            with closing(response["AudioStream"]) as stream:
+                output = os.path.join('tmp', outputfilename + '.mp3')
+                with open(output, "ab") as file:
+                   file.write(stream.read())
+
+
+
+
+pollytext('bookwormvol1.txt','Amy')
+
+
+
+
+
+
+
+
